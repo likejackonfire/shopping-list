583faf2159f5ae356e5dcaf10a8a37717409b403
likejackonfire <gravitybringsmedown@gmail.com>
2019-06-13 17:35:42 -0700

    fixing undefined

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        const listItem = $('.js-shopping-list-entry').val();
+        const listItem = $('#js-shopping-list-entry').val();
         
         $('.shopping-list').append(
             `<li>
